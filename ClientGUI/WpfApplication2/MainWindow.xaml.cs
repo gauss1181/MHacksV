e1e0dd82fa4de21816f192450dbcf3d8830cfc9f
silicarich <nicole.flokos@gmail.com>
2015-01-17 17:49:43 -0500

    ok fixed merge conflict

---
+++ ClientGUI/WpfApplication2/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net.Http;
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Console.Out.WriteLine("hi");
+        }
+
+        //overriding virtual method
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using (var client = new HttpClient())
+            {
+                // want to talk to azure server
+                var responseString = client.GetStringAsync(string.Format("http://oauthbridge.azurewebsites.net/?query={0}", query.Text));
+                textbox1.Text = responseString.Result;
+            }
+
+        }
+
+
+
+    }
+}
